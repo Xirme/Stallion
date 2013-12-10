af49d625fde89e56b65c517f9dc6e635aeef0e24
kdude63 <me@kdude63.com>
2013-12-09 20:15:24 -0800

    Testing the eggs 'n shit.

--- src/me/xir/Main.java
+++ src/me/xir/Main.java
@@ -1,8 +1,51 @@
 package me.xir;
 
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.mcstats.MetricsLite;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main extends JavaPlugin implements Listener {
+
+	protected final Logger logger = Logger.getLogger("Minecraft");
+	protected ItemStack egg;
+
+
+	public void onEnable() {
+		// Register plugin with server
+		getServer().getPluginManager().registerEvents(this, this);
+
+		// Start metrics
+		try {
+			MetricsLite metrics = new MetricsLite(this);
+			metrics.start();
+		} catch (IOException e) {
+			logger.log(Level.SEVERE, e.getMessage());
+		}
+	}
+
+	private void giveEgg(Player player) {
+		player.getInventory().addItem(egg);
+		egg = new ItemStack(Material.MONSTER_EGG, 1, Short.parseShort("100"));
+		egg.setAmount(1);
+		ItemMeta eggMeta = egg.getItemMeta();
+		eggMeta.setDisplayName("Stallion Egg");
+		egg.setItemMeta(eggMeta);
+		player.getInventory().addItem(egg);
+	}
+
+	@EventHandler
+	private void u_wot(PlayerLoginEvent e) {
+		giveEgg(e.getPlayer());
+	}
 
 }
