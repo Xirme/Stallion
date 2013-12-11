abb3cfd160b8885008bfbf123a18c13f2dbe10a9
kdude63 <me@kdude63.com>
2013-12-10 20:14:41 -0800

    Testing more things.

--- src/me/xir/stallion/Main.java
+++ src/me/xir/stallion/Main.java
@@ -4,7 +4,7 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -31,7 +31,7 @@ public class Main extends JavaPlugin implements Listener {
 	}
 
 	@EventHandler
-	private void u_wot(PlayerLoginEvent e) {
+	private void u_wot(AsyncPlayerChatEvent e) {
 		giveEgg(e.getPlayer());
 	}
 
